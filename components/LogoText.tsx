cb7c7fae883035d365518ffb1e1a0e8ad353462a
Mike Melchior <mike.melchior@outlook.com>
2023-07-31 10:48:58 -0400

    set color

--- components/LogoText.tsx
+++ components/LogoText.tsx
@@ -6,7 +6,7 @@ const eduFont = Edu_SA_Beginner({ subsets: ['latin']})
 
 const LogoText = () => {
   return (
-    <h1 className={`${eduFont.className} flex items-center mx-auto text-4xl md:text-6xl font-bold`}>
+    <h1 className={`${eduFont.className} text-black flex items-center mx-auto text-4xl md:text-6xl font-bold`}>
         MEM<Image src={paw}alt='' className='w-[25px] h-[25px] md:w-[45px] md:h-[45px]'/>RY GAME
     </h1>
   )
